e193db64e52096ea31263e849941c1d0294dff6b
antik1108 <itsantikmondal143@gmail.com>
2026-03-27 16:28:02 +0530

    added compsoe

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
+        host: true,
+        port: 5173,
         proxy: {
             '/api': {
-                target: 'http://localhost:5001',
+                target: process.env.API_URL || 'http://localhost:5001',
                 changeOrigin: true,
             }
         }
